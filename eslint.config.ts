d47162cb06fd95c11646b28ed63f4b10434076f2
Jonathan Braaten Mjøs <jonathan@mjos.no>
2026-03-25 22:02:14 +0100

    chore: i eslint, add prettier

---
+++ eslint.config.ts
@@ -0,0 +1,26 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      'no-unused-vars': 'error',
+      'no-console': 'warn',
+    },
+  },
+  tseslint.configs.strict,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+    },
+  },
+])
